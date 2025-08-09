c3cc78920619b24f81079927cd4fe4912fa2212e
Akhila Marisetty <150886952+MAkhi2161@users.noreply.github.com>
2025-08-09 20:58:17 +0530

    Created User.java

---
+++ java/com.example.simpleblog/entity/User.java
@@ -0,0 +1,51 @@
+package com.example.simpleblog.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    private String username;
+
+    private String password;
+
+    private String email;
+
+    // Constructors
+    public User() {}
+
+    public User(String username, String password, String email) {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+    }
+
+    // Getters and setters
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
