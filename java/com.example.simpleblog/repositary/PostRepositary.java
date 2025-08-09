e6d5f90f5410f3c4d810fd4859742349ac875ac5
Akhila Marisetty <150886952+MAkhi2161@users.noreply.github.com>
2025-08-09 20:58:47 +0530

    Created PostRepositary.java

---
+++ java/com.example.simpleblog/repositary/PostRepositary.java
@@ -0,0 +1,12 @@
+package com.example.simpleblog.repository;
+
+import com.example.simpleblog.entity.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface PostRepository extends JpaRepository<Post, Long> {
+    List<Post> findByUsername(String username);
+}
