59a526de6e6008fcaf1bd4cff2e35fa6446a5ead
Akhila Marisetty <150886952+MAkhi2161@users.noreply.github.com>
2025-08-09 20:57:52 +0530

    Created Post.java

---
+++ java/com.example.simpleblog/entity/Post.java
@@ -0,0 +1,66 @@
+package com.example.simpleblog.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Column;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "posts")
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    @Column(length = 2000)
+    private String content;
+
+    private String username; // Author username
+
+    // Constructors
+    public Post() {}
+
+    public Post(String title, String content, String username) {
+        this.title = title;
+        this.content = content;
+        this.username = username;
+    }
+
+    // Getters and setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}
